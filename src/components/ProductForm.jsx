f676224eede9d54d7fedbdd428696df1ad68e18d
josuedurann <josueduran154@gmail.com>
2025-05-20 16:42:24 -0300

    cambios en el productform

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
 
-export const AgregarProducto = () => {
-    const descuento = 21;
-    const precioUnitario = 21;
-    const nuevoProducto = {id: 1 ,descripcion: "nashe", precioUnitario: precioUnitario, descuento: descuento, stock: 21 ,precioConDescuento: precioUnitario*(1-descuento/100) };
+export const Formulario = () => {
+    const [producto, setProducto] = useState({
+        id: '',
+        descripcion: '',
+        precioUnitario: 0,
+        descuento: 0,
+        stock: 0,
+    });
+
     return (
-        <div>
-        <h2>Producto Agregado</h2>
-        <p>Id: ${nuevoProducto.id}</p>
-        <p>Descripcion: ${nuevoProducto.descripcion}</p>
-        <p>Precio Unitario: ${nuevoProducto.precioUnitario}</p>
-        <p>Descuento: ${nuevoProducto.descuento}</p>
-        <p>Stock: ${nuevoProducto.stock}</p>
-        <p>Precio con Descuento: ${nuevoProducto.precioConDescuento}</p>
-        </div>
+        <form>
+            <h2>"Agregar Producto"</h2>
+            
+            <label><imput name="id" type="text" placeholder="ID" value={id} /></label>
+        </form>
         )
 }
 
