f79a5a6964fb8820fae4f8ff22bdd44f9b4b4b5c
josuedurann <josueduran154@gmail.com>
2025-05-20 17:50:52 -0300

    Fix ProductForm anashex

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react';
 
-export const ProductForm = () => {
+
+const ProductForm = ({onSubmit, editingProduct}) => {
+
     const [producto, setProducto] = useState({
         id: '',
         descripcion: '',
@@ -9,13 +11,46 @@ export const ProductForm = () => {
         stock: 0,
     });
 
+
+    useEffect(() => {
+        if (editingProduct){
+            setProducto(editingProduct);
+        }
+    }, [editingProduct]);
+
+
+    useEffect(() => {
+        const precio = parseFloat(producto.precioUnitario);
+        const descuento = parseFloat(producto.descuento);
+        const precioConDescuento = precio - (precio * (descuento / 100));
+        setProducto(prev => ({...prev, precioConDescuento: Number(precioConDescuento.toFixed(2))}));
+    }, [producto.precioUnitario, producto.descuento]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProducto((prev) => ({...prev, [name]: value }));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!producto.id || !producto.descripcion) return; 
+        onSubmit({...producto, precioUnitario: +producto.precioUnitario, descuento: +producto.descuento, stock: +producto.stock});
+        
+        setProducto({ id: '', descripcion: '', precioUnitario: 0, descuento: 0, stock: 0 });
+        }
+      
     return (
-        <form>
-            <h2>"Agregar Producto"</h2>
-            
-            <label><input name="id" type="text" placeholder="ID"/></label>
+        <form onSubmit={handleSubmit}>
+            <h2>{editingProduct ? 'Editar Producto' : 'Agregar Producto' }</h2>
+            <input name="id" value={producto.id} onChange={handleChange} placeholder="ID" disabled={!!editingProduct}/>
+            <input name="descripcion" value={producto.descripcion} onChange={handleChange} placeholder="Descripcion" />
+            <input name="precioUnitario" value={producto.precioUnitario} onChange={handleChange} placeholder="Precio Unitario" /> 
+            <input name="descuento" type="number" value={producto.descuento} onChange={handleChange} placeholder="Descuento %" />
+            <input name="stock" type="number" value={producto.stock} onChange={handleChange} placeholder="Stock" />
+            <button type="submit">{editingProduct ? 'Actualizar' : 'Agregar'}</button>
         </form>
-        )
-}
+        );
+    }
+  
 
 export default ProductForm
